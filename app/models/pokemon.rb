5590119c21c4c100a5991b482c39a797586b85b3
Mark <markchav3z@gmail.com>
2016-08-17 15:43:29 +0800

    [IN PROGRESS] Add `sync` method that integrates with PokeAPI

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,12 @@
 class Pokemon < ActiveRecord::Base
   validates :name, :pokemon_identifier, presence: true
+
+  def sync
+    url = "http://pokeapi.co/api/v2/pokemon/#{name}"
+    response = HTTParty.get(url)
+
+    raise if response.code != 200
+
+    JSON.parse(response.body)
+  end
 end
