3811510d496c97d330c4978f3658b466e656eafb
Mark <markchav3z@gmail.com>
2016-08-18 14:17:29 +0800

    Setup controller and routes for the UI

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,7 @@
 class Pokemon < ActiveRecord::Base
   validates :name, :pokemon_identifier, presence: true
 
+  # TODO: store some attributes if needed
   def sync
     url = "http://pokeapi.co/api/v2/pokemon/#{name}"
     response = HTTParty.get(url)
