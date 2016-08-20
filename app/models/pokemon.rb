5f321236fbee1e8148b815fe4a1fd8d839bfefd5
Mark <markchav3z@gmail.com>
2016-08-20 20:51:18 +0800

    update

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,26 +1,6 @@
 class Pokemon < ActiveRecord::Base
   validates :name, :pokemon_identifier, presence: true
 
-  def sync_types
-    return types if self.types.any?
-
-    url = "http://pokeapi.co/api/v2/pokemon/#{name}"
-    response = HTTParty.get(url)
-
-    raise if response.code != 200
-
-    parsed_response = JSON.parse(response.body)
-
-    # get pokemon types
-    types = parsed_response["types"]
-    types = types.map do |t|
-      t["type"]["name"]
-    end
-    self.update(types: types)
-
-    types
-  end
-  
   def sprite_url
     "pokemons/#{pokemon_identifier}.png"
   end
